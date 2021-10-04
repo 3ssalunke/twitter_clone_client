e68c9782f597ec71d5a07d70b6537366bf30fe23
Chinsan Chung <chinsanchung@gmail.com>
2021-10-04 13:33:00 +0900

    axios.defaults.baseURL 수정
    dynamic port 를 제공하는 heroku 에 맞춰 특정 포트 대신 '/api'만 저장하도록 했습니다.

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:80/api';
+axios.defaults.baseURL = '/api';
 // axios.defaults.baseURL = 'http://localhost:5000/api';
 
 ReactDOM.render(
