d69d9e1f4c142f6f8221073660ef7faef5154ecc
Chinsan Chung <chinsanchung@gmail.com>
2021-10-14 23:54:45 +0900

    불필요한 주석 제거

--- src/util/createRandomTweetId.ts
+++ src/util/createRandomTweetId.ts
@@ -1,5 +1,3 @@
-// 트윗 아이디 만들기. getTime 하고 유저아이디 결합하기?
-// 트윗 아이디 만들기. getTime 하고 유저아이디 결합하기?
 const createRandomTweetId = () => {
   const time: number = new Date().getTime();
   // 출처: https://stackoverflow.com/a/21816629
